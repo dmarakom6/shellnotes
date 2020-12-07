75732b65baf81dd97434c2c2f3afc8eec9e160b5
dmarakom6 <dmarakom@gmail.com>
2020-12-07 13:55:20 +0200

    Updated shellnotes to version 1.0 Shellnotes has just become even more dynamic and smart! Additions: 	-Added quickread: Read notes from terminal 	-Added listnotes: List your notes 	-Added delnote: Delete a note from your folder 	-Added Help Page (help.txt) 	-Quicknote will now ask you to read a note if it already exists 	-Readnote will now ask you to write a new note if it doesn't exist 	-Shellnotes is now able to make a Notes folder if the user hasn't made one 	-Shellnotes now knows who you are and where you are! The program copies the user's name and directory to help him take notes faster and safer 	-You can now use different text editors for taking your notes
    Have fun taking notes!

--- shellnotes.sh
+++ shellnotes.sh
@@ -1,52 +1,146 @@
 #!/bin/bash
+#Change the first line if you don't use bash, in order for the code to work properly.
 #Warning! If you don't have gedit(text editor) or nano installed, you may have to change the default text editor (lines 10, 25, 40).
-#I don't usually code in shell, I just made this to keep notes i want faster. So please don't judge me!
-#My tool for making quick notes via terminal, including only 3 simple functions:
+#remember to write this line into your terminal
+#chmod +x shellnotes.sh
+
+
+#Make a Notes folder for the user
+cd ~
+if [[ -d "Notes" ]]; then
+	exists=True;
+else
+	mkdir Notes;
+fi
+
 
 
 #opening a note (newnote will work as well, but this is a simpler one, and will create a new note if $notename doesn't exist)
 function readnote() { 
-	cd Notes; 
-	read -p "Enter note name: " opennote; 
-	nano $opennote; 
-	clear; }
+dir="$(pwd)";
+me="$(whoami)";
+read -p "Enter note name: " opennote; 
+cd ~/Notes;
+if [ -e $opennote ]; then
+	gedit $opennote;
+	
+else
+	clear;
+	echo "No such note.";
+	cd ~
+	read -p "Do you want to create one?[Y/N]: " create;
+	
+fi
+	if [ $create == "y" ] || [ $create == "Y" ] || [ $create == "yes" ] || [ $create == "YES" ] || [ $create == "Yes" ]; then
+		newnote
+	fi
+	
+clear;
+cd $dir
 
+}
 
+#Read notes instantly via terminal
+function quickread() {
+	dir="$(pwd)";
+	read -p "Enter note name: " quicknotename;
+	cd ~/Notes
+	if [ -e $quicknotename ]; then
+		clear;
+		cat $quicknotename
+	
+	else
+		clear;
+		echo "No such note.";
+		cd ~
+		
+fi
 
+cd $dir
 
-
+}
 
 #A quick solution to users who want keeping notes instantly via terminal.
-	function quicknote() { 
-	pwd; 
-	ls -l; 
-	mkdir Notes
-	cd Notes; 
-	read -p "Enter note name: " notename; 
+function quicknote() {
+dir="$(pwd)"; 
+me="$(whoami)";
+cd ~/Notes;
+read -p "Enter note name: " notename;
+if [ -e $notename ]; then
+	clear;
+	echo "This note already exists.";
+	cd ..
+	read -p "Do you want to read it?[Y/N]: " readquicknote;
+	
+else
 	touch $notename; 
 	nano $notename; 
-	cd ..; 
-	clear; 
-	echo "Note created in Home/dimitris/Notes"; 
+	cd $dir;
+	clear;
+	echo "Note created in Home/$me/Notes"; 
 	echo "-----------------------------------"; 
+		
+fi
+
+if [ $readquicknote == "y" ] || [ $readquicknote == "Y" ] || [ $readquicknote == "yes" ] || [ $readquicknote == "YES" ] || [ $readquicknote == "Yes" ]; then
+	clear;
+	cd ~/Notes
+	cat $notename;
+	cd $dir
+fi
 }
 
 
-
-
-
-#like quicknote(), but it openg ubuntu's text-editor.
+#like quicknote(), but it opens ubuntu's text-editor (gedit).
 function newnote() { 
-	pwd; 
-	ls; 
-	mkdir Notes
-	cd Notes; 
-	gedit; 
-	clear; 
-	cd ..; 
-	clear; 
-	echo "Note created in Home/dimitris/Notes"; 
-	echo "-----------------------------------"; 
+cd ~/Notes; 
+dir="$(pwd)";
+me="$(whoami)";
+gedit; 
+clear; 
+cd $dir; 
+echo "Note created in Home/$me/Notes"; 
+echo "-----------------------------------"; 
 }
 
+#Delete notes from terminal
+function delnote() {
+	cd ~/Notes
+	me="$(whoami)";
+	dir="$(pwd)";
+	read -p "Enter the name of the note you want to delete: " delete
+	if [ -e $delete ]; then
+		rm $delete
+		clear;
+		cd $dir;
+		echo "Note deleted from Home/$me/Notes"; 
+		echo "-----------------------------------";
+	else
+		echo "No such file."
+		cd ~;
+	fi
 
+}
+
+#list your notes via terminal. 
+function listnotes() {
+	ls ~/Notes -t
+}
+
+#Help for new users
+function shellnotes() {
+	if [[ $1 == "-v" ]]; then
+		echo "Shellnotes version: 1.0"
+
+	elif [[ $1 == "-r" ]]; then
+		echo "Github repository: https://github.com/dmarakom6/shellnotes/blob/master/"
+
+	elif [[ $1 == "-h" ]]; then
+		less ~/.help;
+		cd $dir;
+	elif [[ $1 == "--help" ]]; then
+		less ~/.help;
+		cd $dir;
+	fi
+}
+#Shellnotes made by dmarakom6.
