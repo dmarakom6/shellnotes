1338d97dc4fed080b455e72e23ef2ec39a2c2807
dmarakom6 <dmarakom@gmail.com>
2021-02-01 17:39:16 +0200

    Add defaults in "def/"

---
+++ def/defaults.sh
@@ -0,0 +1,11 @@
+:<<'info'
+shellnotes - defaults.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+NOTES_EDITOR="$(eval cat ~/.shellnotes/util/sd/sd-input1.txt)"
+QUICK_NOTES_EDITOR="$(eval cat ~/.shellnotes/util/sd/sd-input2.txt)"
+DEFAULT_PATH="$(eval echo $(cat ~/.shellnotes/util/sd/sd-input3.txt))"
+
