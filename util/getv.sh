72c38ee47ac4777029b5ae347935c827c7423582
dmarakom6 <dmarakom@gmail.com>
2021-02-02 19:00:42 +0200

    getv: get the current version Used in every startup all utility files will now go to util/

---
+++ util/getv.sh
@@ -0,0 +1,8 @@
+:<<'info'
+shellnotes - getv.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+VER=$(cat ~/.shellnotes/ver/.shellnotes_version)
