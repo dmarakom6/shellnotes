911c91d0b65b512d80f0af7252dc3ec8b23e1013
dmarakom6 <dmarakom@gmail.com>
2021-02-10 18:10:49 +0200

    readnote: Read a note

---
+++ util/readnote.sh
@@ -0,0 +1,36 @@
+:<<'info'
+shellnotes - readnote.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+function readnote() { 
+	dir="$(pwd)"
+	me="$(whoami)"
+	if [ $# -eq 0 ]; then
+		echo -n "Enter note name: " && read notename
+	else
+		notename=$1
+	fi
+
+	cd $DEFAULT_PATH
+	if [ -e $notename ]; then
+		chmod 0444 $notename
+		$NOTES_EDITOR $notename
+		chmod +rw $notename
+		
+	else
+		clear
+		echo "No such note."
+		echo -n "Do you want to create one?[Y/N]: " && read create;
+	fi
+		
+	case $create in y|Y|YES|Yes|yes )
+		$NOTES_EDITOR
+	esac
+		
+	clear
+	cd $dir
+
+}
