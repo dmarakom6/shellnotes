1bfe36d6c94857119a3edb9ddb2efc0cfc72446c
dmarakom6 <dmarakom@gmail.com>
2021-02-19 11:57:54 +0200

    "f" parameter

---
+++ util/listnotes/f/f.sh
@@ -0,0 +1,8 @@
+:<<'info'
+shellnotes - f.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+echo "Current Notes folder: $DEFAULT_PATH"
