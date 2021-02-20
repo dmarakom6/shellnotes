f1d9343d5521e51a2b05417ba5e2a524c71c2ac1
dmarakom6 <dmarakom@gmail.com>
2021-02-20 10:47:46 +0200

    "n" parameter for listnotes

---
+++ util/listnotes/n/n.sh
@@ -0,0 +1,11 @@
+:<<'info'
+shellnotes - n.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+folder="$(cat ~/.shellnotes/util/sd/sd-input3.txt)"
+files="$(eval echo $(cat ~/.shellnotes/util/sd/sd-input3.txt) | xargs ls | wc -l)"
+
+echo "Found $files files in $folder."
