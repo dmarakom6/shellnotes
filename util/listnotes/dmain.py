a0a030a8c78ab90962004559e614eaf21737014a
Dimitris Marakomihelakis <dmarakom@gmail.com>
2021-02-22 10:09:54 +0200

    Delete dmain.py

--- util/listnotes/dmain.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-#shellnotes - dmain.py
-#(C) Dimitris Marakomihelakis
-#Released under the "All rights reserved" category. See the RIGHTS.txt file
-#in /docs/github/ for its full text.
-
-from d import d
-
