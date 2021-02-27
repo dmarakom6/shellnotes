ea38fb24a30a61d4de5d5dc214508f4d38a09dcd
dmarakom6 <dmarakom@gmail.com>
2021-02-27 12:45:11 +0200

    'h' parameter

---
+++ util/h/h.sh
@@ -0,0 +1,11 @@
+:<<'info'
+shellnotes - h.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+case $1 in -h|--help )
+	less ~/.shellnotes/docs/help
+	cd $dir
+esac
