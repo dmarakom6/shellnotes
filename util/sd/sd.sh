33effe09239d205fbbb64a86271140fd99fe78a1
dmarakom6 <dmarakom@gmail.com>
2021-03-24 14:16:06 +0200

    'sd' parameter

---
+++ util/sd/sd.sh
@@ -0,0 +1,10 @@
+:<<'info'
+shellnotes - sd.sh
+(C) Dimitris Marakomihelakis
+Released under the "All rights reserved" category. See the RIGHTS.txt file
+in /docs/github/ for its full text.
+info
+
+case $1 in -sd|--set-defaults )
+	. ~/.shellnotes/set_defaults.sh
+esac
