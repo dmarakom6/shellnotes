cb61e38aa07b325ec3b7ba6264f58879503f4fd1
dmarakom6 <dmarakom@gmail.com>
2021-04-26 09:31:34 +0300

    Change listnotes listing style Rewrote the listnotes command and changed its listing style.

--- util/listnotes.sh
+++ util/listnotes.sh
@@ -6,20 +6,40 @@ in /docs/github/ for its full text.
 info
 
 function listnotes() {
-	case $1 in -d|--dynamic )
-		. ~/.shellnotes/util/listnotes/d/d.sh
-	esac
-	case $1 in -n|--number )
-		. ~/.shellnotes/util/listnotes/n/n.sh
-	esac
-	case $1 in -f|--folder )
-		. ~/.shellnotes/util/listnotes/f/f.sh
-	esac
-	if [[ "$1" == "" ]]; then
-		if [ -z "$(ls -A $DEFAULT_PATH)" ]; then
-			echo "Your Notes folder is empty."
-		else
-			ls $DEFAULT_PATH -t
+
+	listnotesFunc () {
+
+		NOTES="`ls $DEFAULT_PATH --color=always `"
+
+		if [ "$NOTES" ]
+		then
+		    echo "Your notes from ${DEFAULT_PATH}:\n"
+		    echo "$NOTES\
+
+		"
 		fi
-	fi
+	}
+
+	case $1 in
+		-d|--dynamic)
+			. ~/.shellnotes/util/listnotes/d/d.sh
+			;;
+		-n|--number )
+			. ~/.shellnotes/util/listnotes/n/n.sh
+			;;
+		-f|--folder )
+			. ~/.shellnotes/util/listnotes/f/f.sh
+			;;
+		*)
+			if [ -z $1 ]; then
+				if [ -z "$(ls -A $DEFAULT_PATH)" ]; then
+					echo "Your Notes folder is empty."
+				else
+					listnotesFunc
+				fi
+			else
+				echo "Invalid parameter."
+			fi
+	esac
+	return 0
 }
