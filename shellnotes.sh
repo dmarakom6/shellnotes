414e12f7fa83db5e45f43b9a37b29b8e401be676
Dimitris Marakomihelakis <dmarakom@gmail.com>
2021-06-15 14:14:08 +0300

    Remove comment misdirection

--- shellnotes.sh
+++ shellnotes.sh
@@ -1,7 +1,7 @@
 
 
 #The first line is the user's default shell found during installation process.
-#Warning! If you don't have gedit(text editor) or nano installed, you may have to change the default text editor (lines 10, 25, 40).
+#Warning! If you don't have gedit(text editor) or nano installed, you may have to change the default text editor.
 
 #The lines below must NOT be changed.
 DIR="$(pwd)"
