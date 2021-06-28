c453b82bc30e287b26a4e3e22bfa596c680ae5a0
dmarakom6 <dmarakom@gmail.com>
2021-06-28 13:49:55 +0300

    Ask user to show lines (cl)

--- util/notegrep/cl.sh
+++ util/notegrep/cl.sh
@@ -5,12 +5,26 @@ Released under the "All rights reserved" category. See the RIGHTS.txt file
 in /docs/github/ for its full text.
 info
 
-out="$(grep $regex $DEFAULT_PATH/$notename | wc -l)"
+if [ -f $DEFAULT_PATH/$notename ]; then
 
-if [ $out -eq 1 ]; then
-	echo "Compressed $out line of grep output."
-elif [ $out -gt 1 ]; then
-	echo "Compressed $out lines of grep output."
+	out="$(grep $regex $DEFAULT_PATH/$notename | wc -l)"
+
+	if [ $out -eq 1 ]; then
+		echo "Compressed $out line of grep output."
+	elif [ $out -gt 1 ]; then
+		echo "Compressed $out lines of grep output."
+		echo -n "Show lines?(Y/N): " && read show
+		case $show in
+			yes | y | Y | YES | Yes )
+				grep -n $regex $DEFAULT_PATH/$notename
+				;;
+		esac
+	else
+		echo "No matching lines found."
+	fi
+
 else
-	echo "No matching files found."
+	echo "No such file."
+	return 0
+
 fi
