586031426ef09f9aac285f1a0d2040c8f3705671
dmarakom6 <dmarakom@gmail.com>
2021-07-06 11:49:01 +0300

    Update .resetshellnotes.sh

--- util/.hidden/.resetshellnotes.sh
+++ util/.hidden/.resetshellnotes.sh
@@ -6,14 +6,44 @@ in /docs/github/ for its full text.
 info
 
 function resetshellnotes() {
-
+	DIR="$(pwd)"
 	proceed() {
 		echo "y\n" | clearlogs >/dev/null
 		echo "" > ~/.shellnotes/util/listnotes/output/*
 		echo "/usr/bin/gedit" > ~/.shellnotes/util/shellnotes/sd/sd-input1.txt
 		echo "/bin/nano"> ~/.shellnotes/util/shellnotes/sd/sd-input2.txt
 		echo "~/Notes" > ~/.shellnotes/util/shellnotes/sd/sd-input3.txt
-      	sed -i '1d' ~/.shellnotes/.shellnotes.sh
+      	reset="
+
+#The first line is the user's default shell found during installation process.
+#Warning! If you don't have gedit(text editor) or nano installed, you may have to change the default text editor (lines 10, 25, 40).
+
+#The lines below must NOT be changed.
+export DIR="$(pwd)"
+. ~/.shellnotes/util/getv.sh
+. ~/.shellnotes/def/defaults.sh
+. ~/.shellnotes/util/ChMkDir.sh
+. ~/.shellnotes/util/readnote.sh
+. ~/.shellnotes/util/quickread.sh
+. ~/.shellnotes/util/quicknote.sh
+. ~/.shellnotes/util/newnote.sh
+. ~/.shellnotes/util/delnote.sh
+. ~/.shellnotes/util/listnotes.sh
+. ~/.shellnotes/util/findnote.sh
+. ~/.shellnotes/util/findmisplacednote.sh
+. ~/.shellnotes/util/renamenote.sh
+. ~/.shellnotes/util/notewc.sh
+. ~/.shellnotes/util/notegrep.sh
+
+
+
+. ~/.shellnotes/util/shellnotes.sh
+. ~/.shellnotes/util/.hidden/.clearlogs.sh
+. ~/.shellnotes/util/.hidden/.resetshellnotes.sh
+
+      	"
+
+      	echo $reset > ~/.shellnotes/.shellnotes.sh
 	}
 
 	case $1 in -f | --force )
