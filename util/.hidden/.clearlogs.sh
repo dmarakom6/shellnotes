c9df85831318ec690bbaa410ecfc0bd82e6e9bec
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-08-20 20:39:35 +0300

    optimize clearlogs

--- util/.hidden/.clearlogs.sh
+++ util/.hidden/.clearlogs.sh
@@ -9,7 +9,11 @@ function clearlogs() {
 	echo -n "Are you sure? " && read input
 	case $input in Y|Yes|yes|y|YES )
 		cd ~/.shellnotes/util/exec
+		chmod +x clearlogs
 		./clearlogs
+		cd ../../logs/sne
+		touch delete # In case directory is already empty
+		/bin/rm -f *
 		echo "[shellnotes] Logfiles cleared."
 	esac
 	cd $DIR
