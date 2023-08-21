839bc273c8a06487d6d81eb211abe0f1f9d27473
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-08-21 21:50:15 +0300

    3.5 -> 4.0

--- whatsnew.sh
+++ whatsnew.sh
@@ -1,6 +1,8 @@
 #See what's new after opening your terminal
 
-echo -n "Shellnotes is running version 3.5 for the first time.\nWant to see what's new?(Y/N): " && read show
+version="4.0"
+
+echo -n "Shellnotes is running version $version for the first time.\nWant to see what's new?(Y/N): " && read show
 
 case $show in
 	y | yes | Y | Yes | YES )
